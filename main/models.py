4133bfd5a27bcb030ab8b228a29cc5a5527c115e
RomanRudin <rudin.roman@bk.ru>
2023-02-16 00:18:45 +0200

    First commit

--- main/models.py
+++ main/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Music(models.Model):
+    name = models.CharField(primary_key=True, max_length=100, name='name')
+    description = models.CharField(max_length=2000, name='description')
+    # date = models.DateField(name="date")
+    # link = models.CharField(max_length=100, name='link')
+    # image = models.ImageField(name='image')
+    # bit = models.FileField(name='bit')
--- main/views.py
+++ main/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from multiprocessing import context
+from pydoc import describe
+from django.http import HttpResponse, Http404
+from django.template import loader
+from django.shortcuts import get_object_or_404, render
+import logging
 
-# Create your views here.
+from .models import Music
+
+def index(request) -> HttpResponse:
+    try:
+        bit = Music.objects.order_by('name') #TODO order_by('date')
+    except Music.DoesNotExist:
+        raise Http404("Music does not exist")
+    else:
+        context = {
+            'bit': bit,
+        }
+        return render(request, 'cv/main.html', context)
