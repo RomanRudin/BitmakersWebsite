bc4e99cc2dabaa81fda505d67da5f52dc8f7f949
RomanRudin <rudin.roman@bk.ru>
2023-02-19 18:25:24 +0200

    A little bit more design solutions were added

--- main/models.py
+++ main/models.py
@@ -11,6 +11,11 @@ class Music(models.Model):
     # image = models.ImageField(name='image')
     # bit = models.FileField(name='bit')
     # bitogg = models.FileField(name='bitogg')
+    license_mo3 = models.BooleanField(name="license_mo3")
+    license_wav = models.BooleanField(name="license_wav")
+    license_trackout = models.BooleanField(name="license_trackout")
+    license_unlimited = models.BooleanField(name="license_unlimited")
+    license_exclisive = models.BooleanField(name="license_exclisive")
 
 class News(models.Model):
     name = models.CharField(primary_key=True, max_length=100, name='name')
